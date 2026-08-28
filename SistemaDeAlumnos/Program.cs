d30da835dfd9ef233f8fac20049dcf57a9a9d281
Facu <facundoracca006@gmail.com>
2026-08-28 17:36:31 -0300

    etapa 1: La primera clase

---
+++ SistemaDeAlumnos/Program.cs
@@ -0,0 +1,26 @@
+﻿using SistemaDeAlumnos;
+Alumno Pepe = new Alumno()
+{
+    Nombre = "Pepe",
+    Legajo = 1,
+    Nota1 = 10.0m,
+    Nota2 = 5.0m,
+};
+
+Alumno Manolo = new Alumno()
+{
+    Nombre = "Manolo",
+    Legajo = 2,
+    Nota1 = 8.0m,
+    Nota2 = 7.0m,
+};
+
+Console.WriteLine($"El alumno {Pepe.Nombre} tiene un legaje N° {Pepe.Legajo}");
+Console.WriteLine();
+Console.WriteLine($"El alumno {Manolo.Nombre} tiene un legaje N° {Manolo.Legajo}");
+
+Pepe.Nombre = "Pepe1111";
+
+Console.WriteLine($"El alumno {Pepe.Nombre} tiene un legaje N° {Pepe.Legajo}");
+Console.WriteLine();
+Console.WriteLine($"El alumno {Manolo.Nombre} tiene un legaje N° {Manolo.Legajo}");
