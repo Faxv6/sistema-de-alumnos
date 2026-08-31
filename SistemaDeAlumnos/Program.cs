394450a8a8809267f61ede65c3ea85b114fb4898
Facu <facundoracca006@gmail.com>
2026-08-31 09:51:30 -0300

    etapa 2: Constructor

--- SistemaDeAlumnos/Program.cs
+++ SistemaDeAlumnos/Program.cs
@@ -1,19 +1,22 @@
 ﻿using SistemaDeAlumnos;
-Alumno Pepe = new Alumno()
-{
-    Nombre = "Pepe",
-    Legajo = 1,
-    Nota1 = 10.0m,
-    Nota2 = 5.0m,
-};
+Alumno Pepe = new Alumno("Pepe", 1, 7.59m, 5.85m);
+//{
+//    Nombre = "Pepe",
+//    Legajo = 1,
+//    Nota1 = 10.0m,
+//    Nota2 = 5.0m,
+//};
 
-Alumno Manolo = new Alumno()
-{
-    Nombre = "Manolo",
-    Legajo = 2,
-    Nota1 = 8.0m,
-    Nota2 = 7.0m,
-};
+Alumno Manolo = new Alumno("Manolo", 2, 10.0m, 8.52m);
+//{
+//    Nombre = "Manolo",
+//    Legajo = 2,
+//    Nota1 = 8.0m,
+//    Nota2 = 7.0m,
+//};
+
+//Alumno AlumnoPrueba = new Alumno();
+//Error CS7036 - Este error aparece porque se esperan recibir los parámetros para instanciar el nuevo objeto, los cuales son los declarados en el constructor. 
 
 Console.WriteLine($"El alumno {Pepe.Nombre} tiene un legaje N° {Pepe.Legajo}");
 Console.WriteLine();
@@ -24,3 +27,6 @@ Pepe.Nombre = "Pepe1111";
 Console.WriteLine($"El alumno {Pepe.Nombre} tiene un legaje N° {Pepe.Legajo}");
 Console.WriteLine();
 Console.WriteLine($"El alumno {Manolo.Nombre} tiene un legaje N° {Manolo.Legajo}");
+
+Console.WriteLine();
+
